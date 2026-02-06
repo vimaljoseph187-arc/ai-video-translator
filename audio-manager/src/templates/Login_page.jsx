cef3a9ddbe5cc5def07f553caf2e924a7897e3b7
Vimal Jose <valuepawn@vimaljose.local>
2026-02-06 14:54:01 +0530

    Update components folder

---
+++ audio-manager/src/templates/Login_page.jsx
@@ -0,0 +1,14 @@
+
+
+function LoginPage(){
+    return (
+        <>
+        <div className="Card">
+            <h1>Login page</h1>
+            
+        </div>
+        </>
+    )
+}
+
+export default LoginPage
